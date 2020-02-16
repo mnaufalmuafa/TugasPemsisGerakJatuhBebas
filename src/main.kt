345bc1ccf7ad68a23f1298607ddbd681bbadb1bf
mnaufalmuafa <naufalmuafa77@gmail.com>
2020-02-16 12:51:40 +0700

    Membuat inputan kecepatan awal dan ketinggian

--- src/main.kt
+++ src/main.kt
@@ -1,24 +1,26 @@
 private var t = 0f
-private var y = 10f
+private var y = 0f
 private var v = 0f
-private val step = 0.1f
+private var step = 0f
 
 fun main() {
-    println("t\t\tv(t)\t\ty(t) (numerik)")
-    var i = 1
+    print("Masukkan nilai y (m) : ")
+    y = readLine()!!.toFloat()
+    print("Masukkan nilai step (sekon) : ")
+    step = readLine()!!.toFloat()
+    println("t\t\t\tv(t)\t\ty(t) (numerik)")
     while (y >= 0 && y(t) >= 0) {
-        print("%.1f".format(t)+"\t\t")
-        print("%.2f".format(v(t))+"\t\t")
         v = v(t)
-        print("%.2f".format(y(t))+"\n")
         y = y(t)
+        print("%.2f".format(t)+"\t\t")
+        print("%.2f".format(v)+"\t\t")
+        print("%.4f".format(y)+"\n")
         t += step
-        i++
     }
 }
 
 fun y(t : Float) : Float {
-    if (y == 10f && t == 0f) {
+    if (t == 0f) {
         return y
     }
     else {
@@ -27,7 +29,7 @@ fun y(t : Float) : Float {
 }
 
 fun v(t : Float) : Float {
-    if (v == 0f && t == 0f) {
+    if (t == 0f) {
         return 0f
     }
     else {
